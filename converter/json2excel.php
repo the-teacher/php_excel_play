637173805a2a14cf28696d3f0ffbbe749e3234a7
Ilya N. Zykin <zykin-ilya@ya.ru>
2013-08-02 15:18:20 +0400

    fin

--- converter/json2excel.php
+++ converter/json2excel.php
@@ -1,12 +1,18 @@
 <?php
-  $json = file_get_contents($TMP_FILE);
-  $data = json_decode($json, true);
+  $json   = file_get_contents($TMP_FILE);
+  $data   = json_decode($json, true);
   $fields = CSV_FIELDS();
 
+  $ExcelFile = read_excel_2007($TMPL_FILE);
+  $sheet = $ExcelFile->setActiveSheetIndex(0);
+
   foreach($data as $index => $item){
-    foreach($fields as $col_index => $name){
-      echo "$name: $item[$name]".PHP_EOL;
+    foreach($fields as $col_index => $col_name){
+      $row_index  = $index + 1;
+      $cell_value = $item[$col_name];
+      $sheet->setCellValueByColumnAndRow($col_index, $row_index, $cell_value);
     }
-    echo PHP_EOL.PHP_EOL;
   }
+
+  write_excel_2007($ExcelFile, $OUT_FILE);
 ?>
